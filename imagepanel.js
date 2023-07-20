8ee466a4413445c774812dc50f68c3f84bf355a4
Can Şafak Çakır <cansafak20047128@gmail.com>
2023-07-20 13:45:53 +0300

    Websitesinin ilk versiyonu yüklendi.

---
+++ imagepanel.js
@@ -0,0 +1,10 @@
+function scrollLeft() {
+    const imagePanel = document.getElementById('image-panel');
+    imagePanel.scrollLeft -= 200; /* 200 piksel sola kaydır */
+  }
+  
+  function scrollRight() {
+    const imagePanel = document.getElementById('image-panel');
+    imagePanel.scrollLeft += 200; /* 200 piksel sağa kaydır */
+  }
+   
